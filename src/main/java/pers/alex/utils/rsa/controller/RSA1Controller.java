ac082ed568c2826ab8488a69a92db4049ab17926
Alex Yang <dasyang5@gmail.com>
2020-04-13 16:53:07 +0800

    init version

---
+++ src/main/java/pers/alex/utils/rsa/controller/RSA1Controller.java
@@ -0,0 +1,8 @@
+package pers.alex.utils.rsa.controller;
+
+/**
+ * @author Alex
+ * @date 4/13/2020 4:50 PM
+ */
+public class RSA1Controller {
+}
---
+++ src/test/java/pers/alex/utils/rsa/RsaApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.alex.utils.rsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
